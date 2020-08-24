d44b7ca5952cf22b1987cdd0390cb21c167259e2
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-08-24 09:19:33 +0300

    Updated userinfo.js

--- commands/server_and_user/user-info.js
+++ commands/server_and_user/user-info.js
@@ -36,6 +36,7 @@ module.exports = {
         .addField('Joined the server At', `${joineddate}\n \u200b`, true)*/
         .addField(`${member.user.tag}'s information`, [
             `**• Status** ${status}`,
+            `**• Nickname** ${member.nickname}`,
             `**• Roles:** <@&${member._roles.join('> <@&')}>`,
             `**• Account Created On:** ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`,
             `**• Joined At:** ${joineddate}`
