73b6e9052141f28ca35e237264bff6711a95464f
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-08-25 10:51:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ client.on("message", async message => {
     if(message.author.bot) return;
     if(!message.guild) return;
     if(!message.content.startsWith(prefix)) return;
-    if(!message.member) message.member = await message.guild.fetchMember(message);
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
