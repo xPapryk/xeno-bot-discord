6d52d46f8aebcf824263143e9d6d0d9fd4953632
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-09-09 17:56:08 +0300

    Changed support link

--- commands/utilities/support.js
+++ commands/utilities/support.js
@@ -13,8 +13,7 @@ module.exports = {
         .setTitle("Xeno Bot Invite")
         .addField("**Links**", [
             `[Xeno Bot](https://discord.com/api/oauth2/authorize?client_id=505454012481667072&permissions=2147483639&scope=bot)`,
-            `[Xeno Music Bot](https://discord.com/oauth2/authorize?client_id=736670982420430906&permissions=2147483639&scope=bot)`,
-            `[Support Server](https://discord.gg/Cp6tgeW)`
+            `[Support Server](https://discord.gg/XVyw2Jd)`
         ])
         .setTimestamp()
         .setFooter("Powered By Xeno", `${client.user.avatarURL()}`)
